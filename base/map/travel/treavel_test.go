f6c3b98ed068625d147aa347f585919e84e2e9f2
SansenWu <yupei.wu@bitmain.com>
2019-07-12 18:17:55 +0800

    map deep learn

---
+++ base/map/travel/treavel_test.go
@@ -0,0 +1,24 @@
+package travel
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestTravel(t *testing.T) {
+	atc := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(len(atc), reflect.TypeOf(atc))
+
+	stc := []int{1, 2, 3}
+	fmt.Println(reflect.TypeOf(stc))
+
+	st2 := make([]int, 0)
+	fmt.Println(reflect.TypeOf(st2))
+
+
+	st := make(map[int]int, 0)
+	fmt.Println(reflect.TypeOf(st))
+	
+	fmt.Println("xxxxx")
+}
